27004e05237f2d259e0876ebeb1a4e2646f49138
Vladimir <justjestervz@gmail.com>
2022-06-29 16:08:19 +0500

    init

---
+++ app/src/main/java/com/example/shoplist/domain/AddshopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoplist.domain
+
+class AddshopItemUseCase {
+
+    fun addShopItem(shopItem: ShopItem){
+
+    }
+}
---
+++ app/src/main/java/com/example/shoplist/domain/EditShopItemUseCase.kt
@@ -0,0 +1,4 @@
+package com.example.shoplist.domain
+
+class EditShopItemUseCase {
+}
